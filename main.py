d2673671728e545e3f9ec7d6eb9f1ca62dcdeb4c
oaao <oliver.antoniu@gmail.com>
2017-04-11 15:44:31 -0400

    renamed io/http.py to io/http_esi.py to prevent aiohttp dependency name conflict

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from io.filesys import write_json
-import juice
-
-
 # replace functionality if necessary
 '''orders_raw, orders_config = juice.market_import('rens')
 orders_structured = juice.market_distill(orders_raw, orders_config)
