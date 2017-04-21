dfc57e7bcdc05ec5b70782cd8166fbca5727f174
oaao <oliver.antoniu@gmail.com>
2017-04-21 19:48:51 -0400

    more explicit hub ID information

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 def get_station(hub_name):
     hubs = {
-        'jita': [10000002, 60003760],
-        'amarr': [10000043, 60008494],
-        'dodixie': [10000032, 60011866],
-        'hek': [10000042, 60005686],
-        'rens': [10000030, 60004588]
+        'jita':    {'region_id': '10000002', 'system_id': '30000142', 'station_id': '60003760'},
+        'amarr':   {'region_id': '10000043', 'system_id': '30002187', 'station_id': '60008494'},
+        'dodixie': {'region_id': '10000032', 'system_id': '30002659', 'station_id': '60011866'},
+        'hek':     {'region_id': '10000042', 'system_id': '30002053', 'station_id': '60005686'},
+        'rens':    {'region_id': '10000030', 'system_id': '30002510', 'station_id': '60004588'}
     }
     hub_id = hubs[hub_name.lower()]
     return hub_id
