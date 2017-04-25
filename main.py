f61df8545e8170cc7947c70759665a74163eb295
oaao <oliver.antoniu@gmail.com>
2017-04-25 07:04:05 -0400

    rev: taking out the trash

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
 # replace functionality if necessary
-'''orders_raw, orders_config = juice.market_import('rens')
-orders_structured = juice.market_distill(orders_raw, orders_config)
-# orders_contextualised = market_context(orders_structured, orders_config)
-
-write_json(orders_structured, 'indented')'''
