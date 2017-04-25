426bc67da09b825f6507ee0547031b1ef336b3e1
oaao <oliver.antoniu@gmail.com>
2017-04-25 17:19:58 -0400

    add: generator for traded typeIDs + file, needed for history

---
+++ p_res/generate_type_ids.py
@@ -0,0 +1,10 @@
+import p_io.filesys
+from seeds import orders_distill
+
+type_ids = [t for x in orders_distill('jita', 40) for t in x.keys()]
+print('\n{} unique typeIDs stored.'.format(len(type_ids)))
+
+
+p_io.filesys.write_json(type_ids, 'type_ids')
+
+
