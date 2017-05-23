783f0063df75b8897299f6db49783a10dfeb45b0
oaao <oliver.antoniu@gmail.com>
2017-05-23 17:04:53 -0400

    rev: do not call list() on dict methods (preserve iter)

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import seeds
 # A. ORDERS: retrieve orders data as often as possible
 # -- single case example: market orders in Rens
 rens_grouped    = seeds.orders_distill('Rens', 10)
-rens_ungrouped  = [x for page in rens_grouped for order in list(page.values()) for x in order]
+rens_ungrouped  = [x for page in rens_grouped for order in page.values() for x in order]
 print('Rens currently has {} traded typeIDs with a total of {} market orders.'
       .format(len(rens_grouped), len(rens_ungrouped)))
 
 # B. TYPE IDs: we eventually want a persistent list of all item types traded in the five main hubs to pass to HISTORY
 # -- single case, non-persistent example: traded type IDs in Rens
-rens_type_ids = [x for order_set in rens_grouped for x in list(order_set.keys())]
+rens_type_ids = [x for order_set in rens_grouped for x in order_set.keys()]
 
 # -- truncate this for example purposes, since we need to rate-limit type ID requests
 example_ids = rens_type_ids[:100]
